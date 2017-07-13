768b2fff7642c7699d2599ef9f5116ea760fe2a1
ElNobDeTfm <ElNobDeTfm@users.noreply.github.com>
2017-07-13 15:11:25 -0300

    desktop notifications

--- transports/notifications/index.js
+++ transports/notifications/index.js
@@ -9,7 +9,12 @@
  */
 const io = require('../../includes/io.js'),
 	util = require('../../includes/util.js'),
-	Transport = require('../transport.js');
+	Transport = require('../transport.js'),
+	notifier = require('node-notifier'),
+	opn = require('opn');
+var config,
+	diff;
+
 
 /**
  * Transport class
@@ -17,6 +22,16 @@ const io = require('../../includes/io.js'),
  * @augments Transport
  */
 class Notifications extends Transport {
+    /**
+     * Class constructor
+     * @constructor
+     */
+
+    constructor(data, info, strings) {
+        super(data, info, 'Notifications', strings);
+        config = data;
+    };
+
 	/**
 	 * Bot calls this method when it wants to send a message
 	 * through a transport.
@@ -24,11 +39,82 @@ class Notifications extends Transport {
 	 * @param {String} message Message to send
 	 */
 	send(message) {
+		const format = this._formatMessage(message);
+		var wikiname = config.name || this.info.general.sitename;
 		notifier.notify({
-			'title': 'WikiaActivityLogger',
-			'message': message,
+			'title': `WikiaActivityLogger - ${wikiname}`,
+			'message': this.parse(format),
+			wait: true
 		});
+		if (diff) {
+			var diffLink = `${this.info.general.server}/?diff=${diff}`;
+			notifier.on('click', function (notifierObject, options) {
+  				opn(diffLink);
+			});
+			diff = null;
+		}
 	}
+
+    /**
+     * Preprocesses the argument passed to the template
+     * @method preprocess
+     * @param {String} arg
+     * @return {String} Preprocessed argument
+     * @throws {Error} If not implemented through subclasses
+     */
+    preprocess(arg) {
+        return String(arg)
+            .replace(/\{/g, '\\{')
+            .replace(/\}/g, '\\}')
+            .replace(/\|/g, 'I')
+    }
+
+    /**
+     * Shorthand for util.linkToArticle
+     * @method _link
+     * @private
+     * @param {String} page Page to link to
+     * @return {String} Link to the page
+     */
+    _link(page) {
+        const general = this.info.general;
+        return util.linkToArticle(general.server, general.articlepath, page);
+    }
+
+    /**
+     * Parses a {{template}}
+     * @method template
+     * @protected
+     * @param {String} name Template name
+     * @param {Array<String>} args Template arguments
+     * @return {String} Parsed template
+     */
+    template(name, args) {
+        switch(name) {
+            case 'diff':
+                const general = this.info.general;
+                diff = args[0];
+                console.log(diff);
+                return '';
+            case 'diffSize':
+                return `(${args[0]})`;
+            case 'user':
+                return `${args[0]}`;
+            case 'link':
+                return `${args[0]}`;
+            case 'userlink':
+                return `${args[0]}`;
+            case 'summary':
+                const a = args[0].trim().replace(/\n/g, '');
+                return (a.length === 0) ? '' : `(${a})`;
+            case 'debug':
+                return `\`\`\`${args[0]}\`\`\``;
+            case 'wiki':
+                return `http://${args[0]}`;
+            case 'board':
+                return `${this.info.namespaces[args[0] - 1]['*']}:${args[1]}`, this._formatMessage([`board-${args[0]}`, args[1]]);
+        }
+    }
 
 }
 
